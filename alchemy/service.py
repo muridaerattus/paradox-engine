692b4e01e70180c410c69f1cfabcdb00144ff657
muridaerattus <muridaerattus@gmail.com>
2025-07-19 16:38:34 -0400

    new strategy

--- alchemy/service.py
+++ alchemy/service.py
@@ -1,9 +1,9 @@
-from alchemy.models import Item, AlchemyCode, Operation, generate_alchemy_code
+from alchemy.models import Item, ItemNameAndDescription, AlchemyCode, Operation, generate_alchemy_code
 from alchemy.operations import alchemy_and, alchemy_or
 from database.alchemy_database import get_item_by_name_or_code, insert_item
 from langchain_together import ChatTogether
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser, PydanticOutputParser
 import aiofiles
 
 async def alchemize_items(item_1_name: str, item_2_name: str, operation: Operation) -> Item:
@@ -20,14 +20,65 @@ async def alchemize_items(item_1_name: str, item_2_name: str, operation: Operati
     item_2 = await get_item_by_name_or_code(item_2_name)
 
     if not item_1:
-        item_1 = await new_item(item_1_name, None, None)
+        item_1 = await new_item(item_1_name)
+        await insert_item(item_1)
     if not item_2:
-        item_2 = await new_item(item_2_name, None, None)
+        item_2 = await new_item(item_2_name)
+        await insert_item(item_2)
 
-    name = await generate_item_name(item_1_name, item_2_name, operation)
-    print(name)
-    description = await generate_description(name, operation)
+    combined_item = await generate_item(item_1, item_2, operation)
+    await insert_item(combined_item)
 
+    return combined_item
+
+async def new_item(name: str) -> Item:
+    """
+    Create a new item with a unique alchemy code.
+    Usually used for common objects that aren't in the database, like "hammer" or "needles".
+    
+    :param name: Name of the item
+    :param description: Description of the item (optional)
+    :return: An Item instance with a generated alchemy code
+    """
+    if not description:
+        description = await generate_description(name)
+    if not code:
+        code = generate_alchemy_code()
+    return Item(name=name, description=description or "", code=code)
+
+async def generate_item(item_1: Item, item_2: Item, operation: Operation) -> Item:
+    """
+    Generate a name for the new item based on the names of the two items being combined.
+    
+    :param name_1: Name of the first item
+    :param name_2: Name of the second item
+    :param operation: The operation used ('and' or 'or')
+    :return: A generated name string
+    """
+    item_name_prompt = None
+    async with aiofiles.open(f'prompts/alchemy/item_generator.md') as f:
+        item_name_prompt = await f.read()
+    
+    prompt = ChatPromptTemplate([
+        ('system', item_name_prompt),
+        ('user', """
+         Item 1: {item_1_name}, {item_1_description}
+         Item 2: {item_2.name}, {item_2.description}
+         Operation: {operation}""")
+    ])
+    llm = ChatTogether(model="meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo")
+    parser = PydanticOutputParser(pydantic_object=ItemNameAndDescription)
+    format_instructions = parser.get_format_instructions()
+    llm_chain = prompt | llm | parser
+
+    item_name_and_description: ItemNameAndDescription = await llm_chain.ainvoke({
+        'item_1_name': item_1.name,
+        'item_1_description': item_1.description,
+        'item_2_name': item_2.name,
+        'item_2_description': item_2.description,
+        'operation': operation,
+        'format_instructions': format_instructions
+    })
     combined_code = None
     match operation:
         case 'and':
@@ -37,62 +88,29 @@ async def alchemize_items(item_1_name: str, item_2_name: str, operation: Operati
         case _:
             raise ValueError(f"Invalid operation: {operation}. Use 'and' or 'or'.")
         
-    combined_item = await new_item(name, description, combined_code)
-    await insert_item(combined_item)
+    return Item(
+        code=combined_code,
+        name=item_name_and_description.name,
+        description=item_name_and_description.description
+    )
 
-    return combined_item
-
-async def new_item(name: str, description: str | None, code: AlchemyCode | None) -> Item:
-    """
-    Create a new item with a unique alchemy code.
-    
-    :param name: Name of the item
-    :param description: Description of the item (optional)
-    :return: An Item instance with a generated alchemy code
-    """
-    if not description:
-        description = await generate_description(name, None)
-    if not code:
-        code = generate_alchemy_code()
-    return Item(name=name, description=description or "", code=code)
-
-async def generate_item_name(name_1: str, name_2: str, operation: Operation) -> str:
-    """
-    Generate a name for the new item based on the names of the two items being combined.
-    
-    :param name_1: Name of the first item
-    :param name_2: Name of the second item
-    :param operation: The operation used ('and' or 'or')
-    :return: A generated name string
-    """
-    llm = ChatTogether(model="meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo")
-    item_name_prompt = None
-    async with aiofiles.open(f'prompts/alchemy/item_name_generator.md') as f:
-        item_name_prompt = await f.read()
-    prompt = ChatPromptTemplate([
-        ('system', item_name_prompt),
-        ('user', f'{name_1}, {name_2}, operation: {operation}')
-    ])
-    llm_chain = prompt | llm | StrOutputParser()
-    return await llm_chain.ainvoke({})
-
-async def generate_description(name: str, operation: Operation | None) -> str:
+async def generate_description(name: str) -> str:
     """
     Generate a description for the item based on its name.
     
     :param name: Name of the item
     :return: A generated description string
-    """
-    if not operation:
-        operation = 'new item'
-    llm = ChatTogether(model="meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo")
+    """    
     item_description_prompt = None
     async with aiofiles.open(f'prompts/alchemy/item_description_generator.md') as f:
         item_description_prompt = await f.read()
+    
     prompt = ChatPromptTemplate([
         ('system', item_description_prompt),
-        ('user', f'{name}, operation: {operation}')
+        ('user', name)
     ])
+    llm = ChatTogether(model="meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo")
     llm_chain = prompt | llm | StrOutputParser()
+
     return await llm_chain.ainvoke({})
     
--- prompts/alchemy/item_generator.md
+++ prompts/alchemy/item_generator.md
@@ -2,25 +2,25 @@
 You are performing "Alchemy", a process of computational arithmetic on items and concepts. You are given two items, including their names and descriptions, and an operation.
 
 <OPERATIONS>
-"AND" is an operation that creates a new object strengthening the qualities that both items share, while reducing the qualities not shared by either.
+"AND" is an operation that creates a new object strengthening the qualities that both predecessor items share, while reducing the qualities not shared by either.
 "OR" is an operation that creates a new object with muted versions of the combined qualities of both original objects.
 </OPERATIONS>
 
 You will perform the specified operation and make a new item, combining the previous two items. You will give this item a name and description.
 
-ome up with a creative, charming, and somewhat funny name for a third item that combines the two in the specified fashion.
+<FORMAT_INSTRUCTIONS>
+{format_instructions}
+</FORMAT_INSTRUCTIONS>
 
-
-
-Write a short 1-2 sentence snappy and funny description of a new item given its name.
-
-Write a short 1-2 sentence snappy and funny description of a new item given its name.
-
-"AND" is an operation that creates a new object strengthening the qualities that both predecessor items share, while reducing the qualities not shared by either.
-"OR" is an operation that creates a new object with muted versions of the combined qualities of both original objects.
-"new item" just makes a new item. No predecessor items.
 </INSTRUCTIONS>
 
-<EXAMPLE>
+<NAME_EXAMPLES>
 Turntables && Laptop = Turntop
-</EXAMPLE>
+</NAME_EXAMPLES>
+
+<DESCRIPTION_EXAMPLES>
+Hammer -> It hits things. Nails, bricks, people. You name it.
+Needles -> For stitching wounds or making new ones. Haystack not included.
+Sword -> Sharp enough to solve most problems. Protip: pointy end goes in the other guy.
+Sickle -> Agriculture's deadliest side hustle. Technically a tool, practically a warning.
+</DESCRIPTION_EXAMPLES>
