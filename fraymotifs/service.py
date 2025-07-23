a99f47b14247b7271126923b7d3220c1bfe952af
muridaerattus <muridaerattus@gmail.com>
2025-07-22 22:50:54 -0400

    test aspect context

--- fraymotifs/service.py
+++ fraymotifs/service.py
@@ -7,6 +7,23 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 import aiofiles
 
+async def generate_aspect_context(titles: list[Title]) -> str:
+    """
+    Retrieve information about the aspects of the given titles.
+    """
+    if not titles:
+        return ""
+    total_aspect_info = ""
+    
+    for title in titles:
+        if not title.title_aspect:
+            raise ValueError("All titles must have an aspect defined.")
+        async with aiofiles.open(f"{PROMPTS_DIRECTORY}/aspects/{title.title_aspect.lower()}.md") as f:
+            aspect_info = await f.read()
+            total_aspect_info += f"Aspect: {title.title_aspect}\nInfo: {aspect_info}\n\n"
+
+    return total_aspect_info.strip()
+
 async def create_fraymotif(titles: list[Title], memory: str, additional_info: str) -> Fraymotif:
     """
     Creates a fraymotif based on the provided titles, memory, and additional information.
@@ -19,12 +36,14 @@ async def create_fraymotif(titles: list[Title], memory: str, additional_info: st
     
     async with aiofiles.open(f"{PROMPTS_DIRECTORY}/fraymotifs/fraymotif_generator.md") as f:
         fraymotif_prompt = await f.read()
+    aspect_context = await generate_aspect_context(titles)
     prompt = ChatPromptTemplate([
         ('system', fraymotif_prompt),
         ('user', """
-         Players: {players}
+         Player Titles: {players}
          Memory: {memory}
-         Additional Info: {additional_info}""")
+         Additional Info: {additional_info}
+         Context for each aspect: {aspect_context}""")
     ])
     llm = ChatAnthropic(model="claude-3-7-sonnet-latest")
     parser = PydanticOutputParser(pydantic_object=Fraymotif)
@@ -35,6 +54,7 @@ async def create_fraymotif(titles: list[Title], memory: str, additional_info: st
         'players': players_formatted,
         'memory': memory,
         'additional_info': additional_info,
+        'aspect_context': aspect_context,
         'format_instructions': format_instructions
     })
     return fraymotif
