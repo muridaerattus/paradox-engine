8871ab5cf232c8aa354837fee4e7af980194485b
muridaerattus <muridaerattus@gmail.com>
2026-01-23 21:51:40 -0500

    fix enable_docs

--- engine/settings.py
+++ engine/settings.py
@@ -10,3 +10,4 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite+aiosqlite:///./paradox.db"
 TERMINAL_URL = os.environ.get("TERMINAL_URL", "http://localhost:3000")
 API_ROOT_PATH = os.environ.get("API_ROOT_PATH", "/api")
 PROMPTS_DIRECTORY = os.environ.get("PROMPTS_DIRECTORY", "prompts")
+ENABLE_DOCS = os.environ.get("ENABLE_DOCS", "false")
